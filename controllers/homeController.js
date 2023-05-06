70621a1854d79198b4a1198360742c3857a10ceb
Shruti <smishra.ninja9252@gmail.com>
2023-05-06 11:09:29 +0530

    Final Styles to HomePage

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -8,14 +8,14 @@ module.exports.home = function(req,res){
 
 module.exports.signIn = function(req,res){
     return res.render('sign-in',{
-        layout : 'homeLayout',
+        layout : 'blank',
         title : 'Sign In'
     })
 }
 
 module.exports.signUp = function(req,res){
     return res.render('sign-up',{
-        layout : 'homeLayout',
+        layout : 'blank',
         title : 'Sign Up'
     })
 }
