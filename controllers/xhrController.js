06d1367a745a1abd8d53cd799228cc14243e30f5
namanTiwari <er.amantiwari.2002@gmail.com>
2023-05-12 01:47:06 +0530

    Fixed bug at Remove Button and Added Volunteer View/Remove Control

--- controllers/xhrController.js
+++ controllers/xhrController.js
@@ -34,3 +34,50 @@ module.exports.viewCompany = async (req,res)=>{
         }
     }
 }
+
+module.exports.viewVolunteer = async function(req,res){
+    console.log(req.query.id);
+
+    try {
+        let allot = await Allotment.findOne({cid : req.query.id})
+    
+        if(allot){
+          if(allot.volunteers.length!=0){
+            await Allotment.findById(allot._id)
+            .then(allots=>{
+                return res.status(200).json({
+                    message : 'Room is Alloted',
+                    data  : allots.volunteers
+                })
+            })
+            .catch(err=>{
+                console.log('Catch error',err);
+            })
+          }else{
+            // room not alloted 
+            return res.status(200).json({
+                message : 'Volunteer Not Alloted',
+                data : null
+            })
+          }
+        }
+    } catch (error) {
+        console.log('Catch error',error);
+    }
+}
+
+module.exports.removeVolunteer =async (req,res)=>{
+    console.log(req.query)
+    try {
+        Allotment.findOne({cid:req.query.id})
+        .then(allot=>{
+            allot.volunteers.pull({_id : allot.volunteers[req.query.index]._id})
+            allot.save();
+            return res.status(200).json({
+                message : 'SuccessFully Deleted The Volunteer'
+            })
+        })
+    } catch (error) {
+        console.log('Catch Error',error);
+    }
+}
